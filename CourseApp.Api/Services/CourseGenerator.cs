0bde0541e34176ad21312a8667cc424a5509d7fa
Daryaskhrv <daryasheqer@yandex.ru>
2026-02-19 22:16:27 +0400

    lab1

---
+++ CourseApp.Api/Services/CourseGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using CourseApp.Domain.Entity;
+
+namespace CourseApp.Api.Services;
+
+public class CourseGenerator
+{
+    private static readonly string[] _courseNames =
+    [
+        "C# для начинающих",
+        "Python для анализа данных",
+        "Микросервисная архитектура",
+        "Docker",
+        "Алгоритмы и структуры данных",
+        "SQL и базы данных",
+        "Автоматизированное тестирование",
+        "HTML+CSS Вёрстка сайтов"
+    ];
+
+    public Course Generate(int id)
+    {
+        var faker = new Faker<Course>("ru")
+            .RuleFor(c => c.Id, _ => id)
+            .RuleFor(c => c.Name, f => f.PickRandom(_courseNames))
+            .RuleFor(c => c.TeacherFullName, f => f.Name.FullName())
+                 //f => $"{f.Name.LastName()} {f.Name.FirstName()} {f.Name.}")
+            .RuleFor(c => c.StartDate, f => f.Date.SoonDateOnly(30))
+            .RuleFor(c => c.EndDate, (f, c) =>
+                c.StartDate.AddDays(f.Random.Int(30, 180)))
+            .RuleFor(c => c.MaxStudents, f => f.Random.Int(10, 50))
+            .RuleFor(c => c.CurrentStudents, (f, c) =>
+                f.Random.Int(0, c.MaxStudents))
+            .RuleFor(c => c.HasCertificate, f => f.Random.Bool())
+            .RuleFor(c => c.Price,
+                f => Math.Round(f.Random.Decimal(5000, 10000), 2))
+            .RuleFor(c => c.Rating, f => f.Random.Int(1, 5));
+
+        return faker.Generate();
+    }
+}
---
+++ CourseApp.Api/Services/CourseService.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using CourseApp.Domain.Entity;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace CourseApp.Api.Services;
+
+public class CourseService(IDistributedCache _cache, IConfiguration _configuration,
+                ILogger<CourseService> _logger, CourseGenerator _generator)
+{
+    public async Task<Course> GetCourseAsync(int id)
+    {
+
+        var cacheKey = $"course-{id}";
+        _logger.LogInformation("Попытка получить курс {CourseId} из кэша",id);
+        var cachedData = await _cache.GetStringAsync(cacheKey);
+
+        if (!string.IsNullOrEmpty(cachedData))
+        {
+            try
+            {
+                var cachedCourse = JsonSerializer.Deserialize<Course>(cachedData);
+
+                if (cachedCourse != null)
+                {
+                    _logger.LogInformation("Курс {CourseId} успешно получен из кэша", id);
+                    return cachedCourse;
+                }
+                _logger.LogWarning("Курс {CourseId} найден в кэше, но десериализация вернула null", id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ошибка десериализации курса {CourseId} из кэша", id);
+            }
+        }
+
+        _logger.LogInformation("Курс {CourseId} отсутствует в кэше. Начинаем генерацию", id);
+
+        var course = _generator.Generate(id);
+        var expirationMinutes = _configuration.GetValue("CacheSettings:ExpirationMinutes", 5);
+
+        var cacheOptions = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(expirationMinutes)
+        };
+
+        await _cache.SetStringAsync(cacheKey, JsonSerializer.Serialize(course), cacheOptions);
+        _logger.LogInformation("Курс {CourseId} сгенерирован и сохранён в кэш", id);
+
+        return course;
+    }
+}
